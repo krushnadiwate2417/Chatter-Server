934e55fc942aafbeb1f91d766a8b8a828933d9f6
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-06-13 17:27:21 +0530

    cors

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const authRouter = require('./controllers/authController');
 const userRouter = require("./controllers/userController");
 const chatRouter = require("./controllers/chatContoller");
 const messageRouter = require("./controllers/messageController");
+const cors = require('cors');
 
 const app = express();
+const allowedOrigins = ['http://localhost:5173'];
+
+app.use(cors({
+    origin : function (origin,callback){
+        if(!origin || allowedOrigins.includes(origin)){
+            callback(null,true);
+        } else {
+            callback(new Error('Not Allowed By CORS'))
+        }
+    },
+    credentials : true
+}));
 
 app.use(express.json());
 app.use("/api/v1/auth",authRouter);
