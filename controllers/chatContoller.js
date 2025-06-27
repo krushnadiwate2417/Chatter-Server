c34c03eaf826f747c070b68bfc209559200fad5d
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-06-27 12:05:58 +0530

    Chat update

--- controllers/chatContoller.js
+++ controllers/chatContoller.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 const Chat = require('./../models/chatSchema');
 const authMiddleware = require('./../middlewares/authMiddleware');
+const Message = require("./../models/messageModel");
 
 
 router.post("/startNewChat",authMiddleware, async(req,res)=>{
     try {
         const chat = new Chat(req.body);
         const savedChat =await chat.save();
-
+        await savedChat.populate('members');
         res.status(201).send({
-            message : "Chat created/started successfully ! ",
+            message : "Chat started successfully ! ",
             success : true,
             data : savedChat
         })
@@ -24,13 +25,45 @@ router.post("/startNewChat",authMiddleware, async(req,res)=>{
 router.get("/getAllChats",authMiddleware, async(req,res)=>{
     try {
         const allChats = await Chat.find({members : {$in : req.user.userId}})
-                        .populate('members').sort({updatedAt : -1});
+                        .populate('members')
+                        .populate('lastMessage')
+                        .sort({updatedAt : -1});
 
         res.status(200).send({
             message : "Chats Fetched successfully ! ",
             success : true,
             data : allChats
         })
+    } catch (error) {
+        res.status(400).send({
+            message : error.message,
+            success : false
+        })
+    }
+})
+
+router.patch("/clearUnreadMessages",authMiddleware, async(req,res)=>{
+        try {
+        const {chatId} = req.body;
+        const chat = await Chat.findOne({_id : chatId}).populate("members").populate("lastMessage");
+        if(!chat){
+            return res.status(400).send({
+            success : false,
+            message : `ChatId ${chatId} : does not exist`,
+        })
+        }
+
+        chat.unReadMessageCount = 0;
+        const upDated = await chat.save();
+        
+        await Message.updateMany({chatId : chatId,read : false},{read : true})
+
+        res.status(200).send({
+            success : true,
+            message : "un-read message cound is 0",
+            data : upDated
+        })
+        
     } catch (error) {
         res.status(400).send({
             message : error.message,
